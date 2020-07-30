37ae8fba1a4baca1d9eb1e884fbe1ff1a51bc980
llaumegui <guillaumelef59@gmail.com>
2020-07-30 23:40:23 +0200

    Starting Controller w/ Wheel Colliders
    Controller's basics :
    Acceleration
    Deceleration
    Turn

---
+++ OffRoad-Project/Assets/Scripts/Controller/CarController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+	public InputManager IM;
+
+	[SerializeField]
+	List<WheelCollider> _throttleWheels;
+	[SerializeField]
+	List<WheelCollider> _steeringWheels;
+
+	public float Torque = 10000f;
+	public float Acceleration = 10f;
+	public float Deceleration = 8f;
+	public float MaxTurn = 20f;
+
+	public float Speed;
+
+	[SerializeField]
+	Rigidbody _rb;
+
+	public Transform CenterOfMass;
+
+	private void Start()
+	{
+		IM = GetComponent<InputManager>();
+		_rb = GetComponent<Rigidbody>();
+
+		if(CenterOfMass != null)
+		{
+			_rb.centerOfMass = CenterOfMass.position;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+
+		foreach (WheelCollider wheel in _throttleWheels)
+		{
+			wheel.motorTorque = Torque * Time.deltaTime * Acceleration * IM.ThrottleInput;
+
+			if (IM.ThrottleInput == 0)
+			{
+				wheel.brakeTorque = Torque * Deceleration * Time.deltaTime;
+			}
+			else
+			{
+				wheel.brakeTorque = 0f;
+			}
+		}
+
+		foreach(WheelCollider wheel in _steeringWheels)
+		{
+			wheel.steerAngle = MaxTurn * IM.SteeringInput;
+		}
+
+		Speed = _rb.velocity.magnitude;
+	}
+}
---
+++ OffRoad-Project/Assets/Scripts/Controller/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	public float ThrottleInput;
+	public float SteeringInput;
+
+	public bool BrakeInput;
+
+	private void Update()
+	{
+		ThrottleInput = Input.GetAxis("Vertical");
+		SteeringInput = Input.GetAxis("Horizontal");
+		BrakeInput = (Input.GetKey(KeyCode.Space));
+
+		if(ThrottleInput <=.1f && ThrottleInput >= -.1f)
+		{
+			ThrottleInput = 0;
+		}
+		if (SteeringInput <= .1f && SteeringInput >= -.1f)
+		{
+			SteeringInput = 0;
+		}
+	}
+}
